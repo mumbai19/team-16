235bf1d17c5bf30db52803502fa8dcfeef67c937
SumeetHaryani <sumeetharyani3@gmail.com>
2019-07-20 18:31:43 +0530

    add video list page

--- PHP/farmer/index.php
+++ PHP/farmer/index.php
@@ -74,6 +74,7 @@
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav mr-auto">
 	        	<li class="nav-item active"><a href="index.html" class="nav-link pl-0">Home</a></li>
+				<li class="nav-item "><a href="videolist.html" class="nav-link pl-0">Videos</a></li>
 	        	<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li>
 	        </ul>

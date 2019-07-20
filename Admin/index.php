3448d8faaf8dc9054edc82c109b7b568841ba8f2
shrutim24 <shrutimore2408@gmail.com>
2019-07-21 03:52:10 +0530

    vendor and admin

--- Admin/index.php
+++ Admin/index.php
@@ -21,140 +21,124 @@
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Colorlib Templates">
-    <meta name="author" content="Colorlib">
-    <meta name="keywords" content="Colorlib Templates">
-    <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <style type="text/css">
+    .hi
+    {
+      margin-top: 10px;
+      width: 900px;
+      height: 300px;
+      margin-left:100px;
+    }
+    .ff
+    {
+      margin-left: 450px;
+      margin-top: 50px;
+      margin-bottom: 50px;
+    }
 
-    <!-- Title Page-->
-    <title>My Website</title>
 
-    <!-- Icons font CSS-->
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <!-- Font special for pages-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-
-    <!-- Vendor CSS-->
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
-
-    <!-- Main CSS-->
-    <link href="css/main.css" rel="stylesheet" media="all">
-    <body>
-          <div class="bg-top navbar-light">
-      <div class="container">
-        <div class="row no-gutters d-flex align-items-center align-items-stretch">
-          <div class="col-md-4 d-flex align-items-center py-4">
-            <a class="navbar-brand" href="index.html">Jaljeevika</a>
-          </div>
-          <div class="col-lg-8 d-block">
-            <div class="row d-flex">
-              <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
-                <div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
-                <div class="text">
-                  <span>Email</span>
-                  <span>jaljeevika@email.com</span>
-                </div>
-              </div>
-              <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
-                <div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
-                <div class="text">
-                  <span>Call</span>
-                  <span>Call Us:022-23333345</span>
-                </div>
-              </div>
-              <div class="col-md topper d-flex align-items-center justify-content-end">
-                <p class="mb-0">
-                  <a href="#" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
-                    <span>Register</span>
-                  </a>
-                  <a href="#" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
-                    <span>Login</span>
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    
+    </style>
+  </head>
+  <body>
+	  <div class="bg-top navbar-light">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch">
+    			<div class="col-md-4 d-flex align-items-center py-4">
+    				<a class="navbar-brand" href="index.html">Jaljeevika</a>
+    			</div>
+	    		<div class="col-lg-8 d-block">
+		    		<div class="row d-flex">
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+					    	<div class="text">
+					    		<span>Email</span>
+						    	<span>jaljeevika@email.com</span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <div class="text">
+						    	<span>Call</span>
+						    	<span>Call Us:022-23333345</span>
+						    </div>
+					    </div>
+					    <div class="col-md topper d-flex align-items-center justify-content-end">
+					    	<p class="mb-0">
+					    		<a href="#" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
+					    			<span>Register</span>
+					    		</a>
+					    		<a href="#" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
+					    			<span>Login</span>
+					    		</a>
+					    	</p>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
     </div>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center px-4">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span>
+	      </button>
+	      <form action="#" class="searchform order-lg-last">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control pl-3" placeholder="Search">
+            <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+          </div>
+        </form>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item active"><a href="" class="nav-link pl-0">Home</a></li>
+	        	<li class="nav-item"><a href="" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="" class="nav-link">Contact Us</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
     
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
-      <div class="container d-flex align-items-center px-4">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="oi oi-menu"></span> Menu
-        </button>
-        
-        <div class="collapse navbar-collapse" id="ftco-nav">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active"><a href="index.html" class="nav-link pl-0">Home</a></li>
-        <li class="nav-item "><a href="videolist.html" class="nav-link pl-0">Videos</a></li>
-            <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li>
-          </ul>
+   
+<section class="home-slider owl-carousel">
+      <div class="slider-item" style="background-image:url(images/f1.jpg);">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 ftco-animate">
+            <h1 class="mb-4">Jaljeevika’s mission is the promotion of aquatic based livelihood and protection of aquatic ecology.</p>
+            <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+          </div>
+        </div>
         </div>
       </div>
-    </nav>
-    <!-- END nav -->
-<?php
 
+      <div class="slider-item" style="background-image:url(images/f2.jpg);">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 ftco-animate">
+            <h1 class="mb-4">We at Jaljeevika work towards promotion of aquatic livelihood.</p>
+            <p><a href="#" class="btn btn-primary px-4 py-3 mt-3">Contact Us</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+<p></p>
 
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "fcfg44";
- 
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
+<div class="ff">
+   <a href="adminacceptuser.php"><button class="btn btn-primary">Accept user</button></a>
+  <button class="btn btn-primary">Add government policies</button>
+		</div>
 
-  //require('../PHP/connection.php');
+		
 
-
-  $selectSQL = "SELECT * FROM users where auth=0 and roleid=3 or roleid =4";
-
- $selectRes = mysqli_query($conn,$selectSQL );
- 
-  if( !$selectRes ){
-    echo 'Retrieval of data from Database Failed - #'.mysqli_errno().': '.mysqli_error();
-    
-  }else{
-    
-    ?>
-<table class="table" border="1" cellspacing="2" cellpadding="2">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Address</th>
-      <th>State</th>
-      <th>District</th>
-      <th>Email Id</th>
-      <th>Accept</th>
-      <th>Reject</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      if( mysqli_num_rows( $selectRes )==0 ){
-        echo '<tr><td colspan="4">No Rows Returned</td></tr>';
-      }else{
-        while( $row = mysqli_fetch_assoc( $selectRes ) ){ ?>
-          <tr><td> <?php echo $row['name'] ?> </td><td> <?php echo $row['address'] ?> </td><td><?php echo $row['state'] ?> </td><td> <?php echo $row['dist'] ?> </td><td><?php echo $row['email'] ?></td><td><button class="btn btn-success">Accept</button></td><td><button class="btn btn-danger">Reject</button></td></tr> <?php
-        }
-      }
-    ?>
-  </tbody>
-</table>
-    <?php
-  }
-?>
-<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+		
+		
+   
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
   <script src="js/jquery.min.js"></script>
@@ -175,4 +159,3 @@
     
   </body>
 </html>
-</body>

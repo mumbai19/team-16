68ea460a3ed2aa6ab0dd2956e160319ca0bd8f39
SumeetHaryani <sumeetharyani3@gmail.com>
2019-07-21 07:25:53 +0530

    integerate graph for analysis

--- PHP/farmer/graphView.php
+++ PHP/farmer/graphView.php
@@ -1,3 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Jaljeevika</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
+
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="ftco-navbar" >
+	    <div class="container d-flex align-items-center px-4">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <form action="#" class="searchform order-lg-last">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control pl-3" placeholder="Search">
+            <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+          </div>
+        </form>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-au">
+            <li class="nav-item "><a href="index.php" class="nav-link pl-0">Home</a></li>
+        <li class="nav-item "><a href="videoFarmerList.php" class="nav-link pl-0">Videos</a></li>
+        <li class="nav-item "><a href="UploadFarmerAssets.php" class="nav-link pl-0">Sell</a></li>
+        <li class="nav-item "><a href="checkVendorAssets.php" class="nav-link pl-0">Buy</a></li>
+        <li class="nav-item "><a href="graphView.php" class="nav-link">Analysis</a></li>
+
+            <li class="nav-item active"><a href="about.html" class="nav-link">About</a></li>
+            <li class="nav-item "><a href="contact.html" class="nav-link">Contact Us</a></li>
+
+          </ul>
+           <ul class="navbar-nav ml-auto">
+      <li class="nav-item mr-2">
+	 <div id="google_translate_element" style="float:right"></div>
+      </li>
+    </ul>
+	      </div>
+	    </div>
+	  </nav>
 <?php
 
 include("fusioncharts.php");
@@ -7,14 +69,14 @@ include("fusioncharts.php");
 <head>
     <title>FusionCharts | My First Chart</title>
 
-    // Include FusionCharts core file
     <script src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
 
-    // Include FusionCharts Theme File
     <script src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
 </head>
 
 <body>
+    <br>
+    <br>
     <?php
 
     $hostdb = "localhost";  // MySQl host
@@ -69,4 +131,28 @@ include("fusioncharts.php");
     </center>
 </body>
 
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+ 
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
 </html>
+
+
